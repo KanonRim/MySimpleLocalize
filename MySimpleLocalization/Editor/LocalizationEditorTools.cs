b0daaed1d683649d12dfef59aa60c7eda53cd8c9
Roman Bashkatov <BashkatovRM@eurochem.ru>
2025-01-26 22:08:10 +0400

    Add .asmdef

--- MySimpleLocalization/Editor/LocalizationEditorTools.cs
+++ MySimpleLocalization/Editor/LocalizationEditorTools.cs
@@ -98,6 +98,5 @@ namespace KoroBox.MySimpleLocalization.Editor
             }
         }
     }
-
-#endif
 }
+#endif
---
+++ MySimpleLocalization/Runtime/MonoBehaviour/MonoLocalizationManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using KoroBox.MySimpleLocalization;
+
+public class  MonoLocalizationManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(LocalizationManager.Instance.Initialize());
+    }
+}
