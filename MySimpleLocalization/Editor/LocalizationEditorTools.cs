e04bcc0c17391853c0349d7e80816c50b7f3f4b9
Roman Bashkatov <BashkatovRM@eurochem.ru>
2025-01-26 22:12:45 +0400

    fix editor assembly

--- MySimpleLocalization/Editor/LocalizationEditorTools.cs
+++ MySimpleLocalization/Editor/LocalizationEditorTools.cs
@@ -1,5 +1,4 @@
-﻿#if UNITY_EDITOR
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -99,4 +98,3 @@ namespace KoroBox.MySimpleLocalization.Editor
         }
     }
 }
-#endif
